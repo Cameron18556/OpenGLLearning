ad9ed66afd304d45e35c8c913d4665e4bea6f442
CameronAIE <82345832+Cameron18556@users.noreply.github.com>
2025-08-24 00:24:36 +0930

    finished lighting lesson

--- Camera.cpp
+++ Camera.cpp
@@ -9,7 +9,7 @@ Camera::Camera(int width, int height, glm::vec3 position)
 }
 
 //updates the camera matrix then sends that to the vertex shader
-void Camera::Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform)
+void Camera::updateMatrix(float FOVdeg, float nearPlane, float farPlane)
 {
 	//initalizes matrices so they are not 0 or null
 	glm::mat4 view = glm::mat4(1.0f);
@@ -20,8 +20,13 @@ void Camera::Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shade
 	//adds perspective
 	projection = glm::perspective(glm::radians(FOVdeg), (float)(width / height), nearPlane, farPlane);
 
+	cameraMatrix = projection * view;
+}
+
+void Camera::Matrix(Shader& shader, const char* uniform)
+{
 	//outputs the camera matrix to the vertex shader
-	glUniformMatrix4fv(glGetUniformLocation(shader.ID, uniform), 1, GL_FALSE, glm::value_ptr(projection * view));
+	glUniformMatrix4fv(glGetUniformLocation(shader.ID, uniform), 1, GL_FALSE, glm::value_ptr(cameraMatrix));
 }
 
 void Camera::Inputs(GLFWwindow* window, float deltaTime)
